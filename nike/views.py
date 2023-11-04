27ce23d3eacb5bc8ab6610c5f6c6323243ce4f5d
wisamosman <viesam_1987@hotmail.com>
2023-11-04 23:02:19 +0100

    add views

--- nike/views.py
+++ nike/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Nike , Review
+
+
+
 
 # Create your views here.
+
+class NikeList(generic.ListView):
+    model = Nike
