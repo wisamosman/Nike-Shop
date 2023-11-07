f849c4f7c4f5c5ccda65db8b5cadddbbf367bf0c
wisamosman <viesam_1987@hotmail.com>
2023-11-07 18:52:40 +0100

    add home.html and show home url in base.html

--- project/urls.py
+++ project/urls.py
@@ -18,11 +18,13 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from settings.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('nike/', include('nike.urls',namespace='nike')),
     path('settings/', include('settings.urls',namespace='settings')),
+    path('',home,name='home'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- settings/views.py
+++ settings/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Company
+from nike.models import Nike, Review
 
 
 
+
+def home(request):
+    nikes = Nike.objects.all()[:8]
+    reviews = Review.objects.all()
+    return render(request,'settings/home.html',{
+        'nikes':nikes,
+        'reviews':reviews,
+    })
 
 # Create your views here.
 
