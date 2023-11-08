690dc1416e397ad8ce3b781e19d3fb3760258bc6
wisamosman <viesam_1987@hotmail.com>
2023-11-08 15:39:35 +0100

    add templates and signup.html for accounts

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Profile,Phones,Address
+from .forms import SignupForm, ActivateUser
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            form.save()
+
+    else:
+        form = SignupForm()
+
+    return render(request,'registration/signup.html',{'form':form})            
