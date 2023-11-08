0d28138ff22a7cb6adf00726de72448c1f053367
wisamosman <viesam_1987@hotmail.com>
2023-11-08 22:07:51 +0100

    edit user templates login+signup

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,6 +3,18 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class SignupForm(UserCreationForm):
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={
+        'class':'form-control',
+        'placeholder':'Username'
+        }
+    ))
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={
+        'class':'form-control',
+        'placeholder':'Password'
+        }
+    ))
     class Meta:
         model = User
         fields = ['username','email','password1','password2']
