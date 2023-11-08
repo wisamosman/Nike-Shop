3556b98e03ad84b055e2443e94e21438ac7ffc05
wisamosman <viesam_1987@hotmail.com>
2023-11-08 23:18:46 +0100

    add cart_contest_processor for orders

---
+++ orders/cart_context_processor.py
@@ -0,0 +1,15 @@
+from .models import Cart, CartDetail
+
+
+
+
+
+def get_cart_data(request):
+    if request.user.is_authenticated:
+        cart , created = Cart.objects.get_or_create(user=request.user,completed=False)
+        cart_detail = CartDetail.objects.filter(cart=cart) 
+
+        return {'cart_data':cart, 'cart_detail_data':cart_detail}
+
+    else:
+        return {}
