f897edaf50cb296f69d0989fb5d0c8869dea7b93
wisamosman <viesam_1987@hotmail.com>
2023-11-08 15:20:55 +0100

    add admin for accounts

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Profile,Phones,Address
+
+
+
 
 # Register your models here.
+
+
+class PhonesAdmin(admin.TabularInline):
+    model = Phones
+
+
+
+class AddressAdmin(admin.TabularInline):
+    model = Address
+
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    pass
+
+
+
+admin.site.register(Profile,ProfileAdmin)
+admin.site.register(Phones)
+admin.site.register(Address)
+
